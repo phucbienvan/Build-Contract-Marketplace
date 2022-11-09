a46820a0ad3ea6295222ef573cd981e72a503cb9
phucbienvan <phuc.bienvan@gmail.com>
2022-11-10 00:28:09 +0700

    func order

---
+++ contracts/marketplace/Marketplace.sol
@@ -0,0 +1,141 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.1;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/structs/Enumerableset.sol";
+
+contract Marketplace is Ownable {
+    using Counters for Counters.Counter;
+    using EnumerableSet for EnumerableSet.AddressSet;
+
+    struct Order {
+        address seller;
+        address buyer;
+        uint256 tokenId;
+        address paymentToken;
+        uint256 price;
+    }
+    Counters.Counter private _orderIdCount;
+    IERC721 public immutable nftContract;
+    mapping (uint256 => Order) orders;
+    uint256 public feeDecimal;
+    uint256 public feeRate;
+    address public feeRecipient;
+    EnumerableSet.AddressSet private _supportedPaymentTokens;
+
+    event OrderAdded(
+        uint256 indexed orderId,
+        address indexed seller,
+        uint256 indexed tokenId,
+        address paymentToken,
+        uint256 price
+    );
+    event OrderCancelled(
+        uint256 indexed orderId
+    );
+    event OrderMatched(
+        uint256 indexed orderId,
+        address indexed seller,
+        address indexed buyer,
+        uint256 tokenId,
+        address paymentToken,
+        uint256 price
+    );
+    event FeeRateUpdate(
+        uint256 feeDecimal,
+        uint256 feeRate
+    );
+
+    constructor (
+        address nftAddress_,
+        uint256 feeDecimal_,
+        uint256 feeRate_,
+        address feeRecipient_
+    ) {
+        require(nftAddress_ != address(0), "invalid nft address");
+        
+        nftContract = IERC721(nftAddress_);
+        _updateFeeRecipient(feeRecipient_);
+        feeDecimal = feeDecimal_;
+        _updateFeeRate(feeDecimal_, feeRate_);
+        _orderIdCount.increment();
+    }
+
+    function _updateFeeRecipient(address feeRecipient_) internal {
+        require(feeRecipient_ != address(0), "invalid fee recipient");
+        feeRecipient = feeRecipient_;
+    }
+
+    function updateFeeRecipient(address feeRecipient_) external onlyOwner {
+        _updateFeeRecipient(feeRecipient_);
+    }
+
+    function _updateFeeRate(uint256 feeDecimal_, uint256 feeRate_) internal {
+        require(feeRate_ < 10**(feeDecimal + 2), "bad fee rate");
+        feeDecimal = feeDecimal_;
+        feeRate = feeRate_;
+        emit FeeRateUpdate(feeDecimal_, feeRate_);
+    }
+
+    function updateFeeRate(uint256 feeDecimal_, uint256 feeRate_) external onlyOwner {
+        _updateFeeRate(feeDecimal_, feeRate_);
+    }
+
+    function _caculateFee(uint256 orderId_) private view returns (uint256) {
+        Order storage _order = orders[orderId_];
+        if (feeRate == 0) {
+            return 0;
+        }
+        return (feeRate * _order.price) / 10**(feeDecimal + 2);
+    }
+
+    function isSeller (uint256 orderId_, address seller_) public view returns (bool) {
+        return orders[orderId_].seller == seller_;
+    }
+
+    function addPaymentToken(address paymentToken_) external onlyOwner {
+        require(paymentToken_ != address(0), "invalid address");
+        require(_supportedPaymentTokens.add(paymentToken_), "payment token supported");
+
+    }
+
+    function isPaymentTokenSupported(address paymentToken_) public view returns (bool) {
+        return _supportedPaymentTokens.contains(paymentToken_);
+    }
+
+    modifier onlySupportedPaymentToken(address paymentToken_) {
+        require(isPaymentTokenSupported(paymentToken_), "unsupported payment token");
+        _;
+    }
+
+    function addOrder(
+        uint256 tokenId_,
+        address paymentToken_, 
+        uint256 price_
+    ) public onlySupportedPaymentToken(paymentToken_) {
+        uint256 _orderId = _orderIdCount.current();
+        orders[_orderId] = Order(
+            _msgSender(),
+            address(0),
+            tokenId_,
+            paymentToken_,
+            price_
+        );
+        nftContract.transferFrom(_msgSender(), address(0), tokenId_);
+        emit OrderAdded(_orderId, _msgSender(), tokenId_, paymentToken_, price_);
+    }
+
+    function cancelOrder(uint256 orderId_) external {
+        Order storage _order = orders[orderId_];
+        require(_order.buyer == address(0), "buyer must be zero");
+        require(_order.seller == _msgSender(), "must be owner");
+        uint256 _tokenId = _order.tokenId;
+        delete orders[orderId_];
+        nftContract.transferFrom(address(this), _msgSender(), _tokenId);
+        emit OrderCancelled(orderId_);
+    }
+}
